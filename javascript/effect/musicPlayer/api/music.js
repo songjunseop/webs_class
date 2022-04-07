903a7abca9e5c23c7285b9d7e22afddc08355a56
songjunseop <97070058+songjunseop@users.noreply.github.com>
2022-04-07 12:59:46 +0900

    2022.04.07

---
+++ javascript/effect/musicPlayer/api/music.js
@@ -0,0 +1,123 @@
+        let allMusic = [
+            {
+                name : "봄여름가을겨울",
+                artist : "BIGBANG (빅뱅)",
+                img : "music01",
+                audio : "music01" 
+            },
+            {
+                name : "TOMBOY",
+                artist : "(여자)아이들",
+                img : "music02",
+                audio : "music02" 
+            },
+            {
+                name : "Feel My Rhythm",
+                artist : "Red Velvet (레드벨벳)",
+                img : "music03",
+                audio : "music03" 
+            },
+            {
+                name : "GANADARA (Feat. 아이유)",
+                artist : "박재범",
+                img : "music04",
+                audio : "music04" 
+            },
+            {
+                name : "사랑인가 봐",
+                artist : "멜로망스",
+                img : "music05",
+                audio : "music05" 
+            },
+            {
+                name : "사랑은 늘 도망가",
+                artist : "임영웅",
+                img : "music06",
+                audio : "music06" 
+            },
+            {
+                name : "LOVE DIVE",
+                artist : "IVE (아이브)",
+                img : "music07",
+                audio : "music07" 
+            },
+            {
+                name : "INVU",
+                artist : "태연 (TAEYEON)",
+                img : "music08",
+                audio : "music08" 
+            },
+            {
+                name : "취중고백",
+                artist : "김민석 (멜로망스)",
+                img : "music09",
+                audio : "music09" 
+            },
+            {
+                name : "듣고 싶을까",
+                artist : "MSG워너비(M.O.M)",
+                img : "music10",
+                audio : "music10" 
+            },
+            {
+                name : "버퍼링 (Glitch Mode)",
+                artist : "NCT DREAM",
+                img : "music11",
+                audio : "music11" 
+            },
+            {
+                name : "RUN2U",
+                artist : "STAYC(스테이씨)",
+                img : "music12",
+                audio : "music12" 
+            },
+            {
+                name : "언제나 사랑해",
+                artist : "케이시 (Kassy)",
+                img : "music13",
+                audio : "music13" 
+            },
+            {
+                name : "이제 나만 믿어요",
+                artist : "임영웅",
+                img : "music14",
+                audio : "music14" 
+            },
+            {
+                name : "ELEVEN",
+                artist : "IVE (아이브)",
+                img : "music15",
+                audio : "music15" 
+            },
+            {
+                name : "다정히 내 이름을 부르면",
+                artist : "경서예지, 전건호",
+                img : "music16",
+                audio : "music16" 
+            },
+            {
+                name : "SMILEY (Feat. BIBI)",
+                artist : "YENA (최예나)",
+                img : "music17",
+                audio : "music17" 
+            },
+            {
+                name : "존재만으로",
+                artist : "원슈타인",
+                img : "music18",
+                audio : "music18" 
+            },
+            {
+                name : "신호등",
+                artist : "이무진",
+                img : "music19",
+                audio : "music19" 
+            },
+            {
+                name : "STAY",
+                artist : "The Kid LAROI, Justin Bieber",
+                img : "music20",
+                audio : "music20" 
+            },
+
+        ]
