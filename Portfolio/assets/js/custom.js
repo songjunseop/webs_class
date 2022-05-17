355150f1301d2181c7d3d867878c914ff489ae4b
songjunseop <apdlvmf8044@gmail.com>
2022-05-17 16:42:32 +0900

    20220517

--- Portfolio/assets/js/custom.js
+++ Portfolio/assets/js/custom.js
@@ -2,6 +2,5 @@
 const menuList = document.querySelectorAll(".menuBox__list li a");
 
 document.querySelector(".nav__bar").addEventListener("click", () => {
-    document.querySelector(".nav__bar").classList.toggle("active");
-})
-
+  document.querySelector(".nav__bar").classList.toggle("active");
+});
