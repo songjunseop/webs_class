3fdbb654e981536548f1572128818832f1bd9200
songjunseop <apdlvmf8044@gmail.com>
2022-05-18 11:32:02 +0900

    20220518

--- Portfolio/assets/js/custom.js
+++ Portfolio/assets/js/custom.js
@@ -4,3 +4,60 @@ const menuList = document.querySelectorAll(".menuBox__list li a");
 document.querySelector(".nav__bar").addEventListener("click", () => {
   document.querySelector(".nav__bar").classList.toggle("active");
 });
+
+// scroll
+window.addEventListener("scroll", () => {
+  let scrollTop =
+    window.pageYOffset || document.documentElement.scrollTop || window.scrollY;
+  document.querySelector(".scrollTop").innerText = Math.round(scrollTop);
+});
+
+// banner
+gsap.to("#nav", {
+  // top: 24,
+  duration: 2,
+  ease: "power4.out",
+  height: 95 + "%",
+  delay: 3,
+  opacity: 1,
+});
+
+if (window.screen.width <= 1000) {
+  gsap.to("#nav", {
+    width: 96 + "%",
+    duration: 2,
+    ease: "power4.out",
+    delay: 3,
+  });
+}
+
+gsap.to(".circle", {
+  duration: 2,
+  ease: "power4.out",
+  delay: 3,
+  // width: 100 + "vw",
+  // height: 100 + "vw",
+  scale: 1,
+});
+
+gsap.to(".section__box__left", {
+  duration: 3,
+  ease: "power4.out",
+  delay: 1,
+  height: 100 + "%",
+});
+
+gsap.to(".section__box__left02", {
+  duration: 2,
+  ease: "power4.out",
+  delay: 3,
+  width: 100 + "%",
+});
+
+gsap.to(".banner__desc", {
+  ease: "power4.out",
+  delay: 4,
+  duration: 2,
+  y: 0,
+  opacity: 1,
+});
