a97c97fb4d2bbc7c72dd7359eca209062ba0f821
songjunseop <apdlvmf8044@gmail.com>
2022-05-20 11:45:14 +0900

    20220520

--- Portfolio/assets/js/banner.js
+++ Portfolio/assets/js/banner.js
@@ -1,21 +1,4 @@
 // banner
-gsap.to("#nav", {
-  duration: 2,
-  ease: "power4.out",
-  height: 95 + "%",
-  delay: 2,
-  opacity: 1,
-});
-
-if (window.screen.width <= 1000) {
-  gsap.to("#nav", {
-    width: 96 + "%",
-    duration: 1.5,
-    ease: "power4.out",
-    delay: 3,
-  });
-}
-
 gsap.to(".circle", {
   duration: 2,
   ease: "power4.out",
@@ -54,36 +37,35 @@ gsap.to(".section__box__right", {
 });
 
 // 한글자씩 올라오기
-gsap.from(".section__box .section__box__right h4", {
-  opacity: 0,
-});
+// gsap.from(".section__box .section__box__right h4", {
+//   opacity: 0,
+// });
 
-setTimeout(() => {
-  gsap.to(".section__box .section__box__right h4", {
-    opacity: 1,
-  });
-}, 3000);
+// setTimeout(() => {
+//   gsap.to(".section__box .section__box__right h4", {
+//     opacity: 1,
+//   });
+// }, 3000);
 
-gsap.from(".section__box .section__box__right h4 span span", {
-  stagger: {
-    amount: 2,
-  },
-  y: 100,
-  delay: 3,
-  opacity: 0,
-});
-setTimeout(() => {
-  gsap.to(".section__box .section__box__right h4 span span", {
-    stagger: {
-      amount: 2,
-    },
-    y: 0,
-    delay: 3,
-    opacity: 1,
-  });
-}, 3000);
+// gsap.from(".section__box .section__box__right h4 span span", {
+//   stagger: {
+//     amount: 2,
+//   },
+//   y: 100,
+//   delay: 3,
+//   opacity: 0,
+// });
+// setTimeout(() => {
+//   gsap.to(".section__box .section__box__right h4 span span", {
+//     stagger: {
+//       amount: 2,
+//     },
+//     y: 0,
+//     delay: 3,
+//     opacity: 1,
+//   });
+// }, 3000);
 
-// 한번에 올라오기
 // let content = document.querySelectorAll(".banner__title");
 // content.forEach((el) => {
 //   let splitText = el.innerText;
@@ -95,31 +77,32 @@ setTimeout(() => {
 //   el.setAttribute("aria-label", splitText);
 // });
 
-// gsap.to(".section__box__right h4:nth-child(1)", {
-//   ease: "power4.out",
-//   delay: 3,
-//   duration: 2,
-//   y: 0,
-//   opacity: 1,
-// });
-// gsap.to(".section__box__right h4:nth-child(2)", {
-//   ease: "power4.out",
-//   delay: 3.5,
-//   duration: 2,
-//   y: 0,
-//   opacity: 1,
-// });
-// gsap.to(".section__box__right h4:nth-child(3)", {
-//   ease: "power4.out",
-//   delay: 4,
-//   duration: 2,
-//   y: 0,
-//   opacity: 1,
-// });
-// gsap.to(".section__box__right h4:nth-child(4)", {
-//   ease: "power4.out",
-//   delay: 4.5,
-//   duration: 2,
-//   y: 0,
-//   opacity: 1,
-// });
+// 한번에 올라오기
+gsap.to(".section__box__right h4:nth-child(1)", {
+  ease: "power4.out",
+  delay: 3,
+  duration: 1,
+  y: 0,
+  opacity: 1,
+});
+gsap.to(".section__box__right h4:nth-child(2)", {
+  ease: "power4.out",
+  delay: 3.5,
+  duration: 1,
+  y: 0,
+  opacity: 1,
+});
+gsap.to(".section__box__right h4:nth-child(3)", {
+  ease: "power4.out",
+  delay: 4,
+  duration: 1,
+  y: 0,
+  opacity: 1,
+});
+gsap.to(".section__box__right h4:nth-child(4)", {
+  ease: "power4.out",
+  delay: 4.5,
+  duration: 1,
+  y: 0,
+  opacity: 1,
+});
