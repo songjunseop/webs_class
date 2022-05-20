8d9d9a7d560df3b22ace12e29f8e2c2e3975fb64
songjunseop <apdlvmf8044@gmail.com>
2022-05-20 15:59:57 +0900

    20220520

--- Portfolio/assets/js/works.js
+++ Portfolio/assets/js/works.js
@@ -1,1 +1,16 @@
 gsap.registerPlugin(ScrollTrigger);
+// let SECTIONS = gsap.utils.toArray(".rail__blue > p");
+
+// gsap.to(SECTIONS, {
+//   xPercent: -100 * (SECTIONS.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".rail__blue",
+//     markers: true,
+//     start: "center 35%",
+//     end: () => "+=" + document.querySelector(".rail__blue > p").offsetWidth,
+//     pin: false,
+//     scrub: 1,
+//     snap: 1 / (SECTIONS.length - 1),
+//   },
+// });
