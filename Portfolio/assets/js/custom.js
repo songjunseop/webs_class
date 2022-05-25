d68c0fbb95ee370fcc53c819f999cde535b5094f
songjunseop <97070058+songjunseop@users.noreply.github.com>
2022-05-25 10:42:19 +0900

    20220525

--- Portfolio/assets/js/custom.js
+++ Portfolio/assets/js/custom.js
@@ -5,13 +5,21 @@
 //   document.querySelector(".nav__bar").classList.toggle("active");
 // });
 
-// gsap.to("#nav", {
-//   duration: 2,
-//   ease: "power4.out",
-//   height: 95 + "%",
-//   delay: 2,
-//   opacity: 1,
-// });
+gsap.to("#nav", {
+  duration: 2,
+  ease: "power4.out",
+  height: 95 + "%",
+  delay: 2,
+  opacity: 1,
+});
+
+gsap.to(".nav__bar", {
+  duration: 2,
+  ease: "power4.out",
+  height: 100 + "%",
+  delay: 2,
+  opacity: 1,
+});
 
 var menuBtn = document.querySelector(".menu-btn");
 var nav = document.querySelector("nav");
@@ -27,26 +35,26 @@ menuBtn.addEventListener("click", () => {
   link.classList.toggle("fade-in");
 });
 
-// function resize() {
-//   if (window.innerWidth <= 1000) {
-//     gsap.to("#nav", {
-//       width: 96 + "%",
-//       height: 80,
-//       duration: 0,
-//       ease: "power4.out",
-//     });
-//   } else {
-//     gsap.to("#nav", {
-//       duration: 0,
-//       ease: "power4.out",
-//       width: 80,
-//       height: 95 + "%",
-//       opacity: 1,
-//     });
-//   }
-//   requestAnimationFrame(resize);
-// }
-// resize();
+function resize() {
+  if (window.innerWidth <= 1000) {
+    gsap.to("#nav", {
+      width: 96 + "%",
+      height: 80,
+      duration: 0,
+      ease: "power4.out",
+    });
+  } else {
+    gsap.to("#nav", {
+      duration: 0,
+      ease: "power4.out",
+      width: 80,
+      height: 95 + "%",
+      opacity: 1,
+    });
+  }
+  requestAnimationFrame(resize);
+}
+resize();
 
 // scroll
 // window.addEventListener("scroll", () => {
