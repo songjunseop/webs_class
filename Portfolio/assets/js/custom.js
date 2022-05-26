590356deab1516c260e56df5f7c2729d5e779274
songjunseop <97070058+songjunseop@users.noreply.github.com>
2022-05-26 16:53:25 +0900

    20220526

--- Portfolio/assets/js/custom.js
+++ Portfolio/assets/js/custom.js
@@ -14,23 +14,70 @@ menuBtn.addEventListener("click", () => {
   link.classList.toggle("fade-in");
 });
 
-// function resize() {
-//   if (window.innerWidth <= 1000) {
-//     gsap.to("#nav", {
-//       width: 96 + "%",
-//       height: 80,
-//       duration: 0,
-//       ease: "power4.out",
-//     });
-//   } else {
-//     gsap.to("#nav", {
-//       duration: 0,
-//       ease: "power4.out",
-//       width: 80,
-//       height: 95 + "%",
-//       opacity: 1,
-//     });
-//   }
-//   requestAnimationFrame(resize);
-// }
-// resize();
+function resize() {
+  if (window.innerWidth <= 1000) {
+    if (document.querySelector(".nav__bar").classList.contains("nav-open")) {
+      gsap.to("#header", {
+        width: 96 + "%",
+        height: 400,
+      });
+      gsap.to(".nav__bar", {
+        width: 100 + "%",
+        height: 400,
+      });
+      gsap.to(".menu-btn", {
+        top: 30,
+        right: 5 + "%",
+      });
+    } else {
+      gsap.to("#header", {
+        width: 96 + "%",
+        height: 80,
+      });
+      gsap.to(".nav__bar", {
+        width: 100 + "%",
+        height: 80,
+      });
+      gsap.to(".menu-btn", {
+        top: 30,
+        right: 5 + "%",
+      });
+      gsap.to(".nav__text", {
+        top: 6 + "%",
+      });
+    }
+  } else {
+    if (document.querySelector(".nav__bar").classList.contains("nav-open")) {
+      gsap.to(".nav__bar", {
+        width: 400,
+        duration: 1,
+        ease: "power4.out",
+      });
+      gsap.to(".nav__text", {
+        top: 6.8 + "%",
+      });
+      gsap.to(".menu-btn", {
+        top: 8 + "%",
+        right: 8 + "%",
+      });
+      // gsap.to(".nav__text", {
+      //   top: 6.5 + "%",
+      // });
+    } else {
+      gsap.to("#header", {
+        width: 80,
+        height: 95 + "%",
+      });
+      gsap.to(".nav__bar", {
+        width: 100 + "%",
+        height: 100 + "%",
+      });
+      gsap.to(".menu-btn", {
+        top: 8 + "%",
+        right: 30 + "%",
+      });
+    }
+  }
+  requestAnimationFrame(resize);
+}
+resize();
